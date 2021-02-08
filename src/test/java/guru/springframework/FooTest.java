9deb504f2c2a6722c149f37f1742c38ff2c78cdc
chemelson <ross03@mail.ru>
2021-02-08 15:50:51 +0300

    Foobar test

---
+++ src/test/java/guru/springframework/FooTest.java
@@ -0,0 +1,18 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FooTest {
+
+    @Test
+    void getBar() {
+        Foo foo = new Foo();
+
+        String result = foo.getBar();
+
+        assertEquals("Foobar", result);
+    }
+
+}
